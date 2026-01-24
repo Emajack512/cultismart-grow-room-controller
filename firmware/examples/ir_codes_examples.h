b10473cb9dd025a9703cf60748aa6a71454eb431
Emanuel López <loezemanuel8@gmail.com>
2026-01-23 22:40:48 -0300

    Refactor IR codes and update constants

---
+++ firmware/examples/ir_codes_examples.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Arduino.h>
+
+// Replace this file by copying it to ir_codes.h and pasting your real codes (DO NOT commit ir_codes.h).
+
+static const uint16_t IR_AIRE1_ON[]   = { /* ... */ };
+static const uint16_t IR_AIRE1_OFF[]  = { /* ... */ };
+static const uint16_t IR_AIRE1_UP[]   = { /* ... */ };
+static const uint16_t IR_AIRE1_DOWN[] = { /* ... */ };
+
+static const uint16_t IR_AIRE2_ON[]   = { /* ... */ };
+static const uint16_t IR_AIRE2_OFF[]  = { /* ... */ };
+static const uint16_t IR_AIRE2_UP[]   = { /* ... */ };
+static const uint16_t IR_AIRE2_DOWN[] = { /* ... */ };
+
+static constexpr int IR_AIRE1_LEN = 0;
+static constexpr int IR_AIRE2_LEN = 0;
+static constexpr int IR_KHZ = 38;
